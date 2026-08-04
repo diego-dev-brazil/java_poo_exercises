328e304144cdaea4b8231bc07746c19a054a2cf6
diego <diegoaugustooliveira3@gmail.com>
2026-08-04 08:44:21 -0300

    Exercício final de arquivos, iniciando módulo de interfaces

--- Arquivos/introducao_arquivos/src/exercicio01/Program.java
+++ Arquivos/introducao_arquivos/src/exercicio01/Program.java
@@ -1,10 +1,14 @@
 package exercicio01;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
@@ -13,10 +17,17 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		String strPath = sc.nextLine();
 		File path = new File(strPath);
+		List<String> writeLines = new ArrayList<>();
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String result = br.readLine();
-			String[] fields = result.split(",");
-			
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] fields = line.split(",");
+				String name = fields[0];
+				Double price = Double.parseDouble(fields[1]);
+				Integer quantity = Integer.parseInt(fields[2]);
+				String newLine = (name + ", " + (price * quantity));
+				writeLines.add(newLine);
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -24,6 +35,20 @@ public class Program {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		String strwPath = path.getParentFile() + "/out";
+		File wpath = new File(strwPath);
+		wpath.mkdir();
+		strwPath += "/summary.csv";
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(strwPath))){
+			for (String line : writeLines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sc.close();
 	}
 
 }
