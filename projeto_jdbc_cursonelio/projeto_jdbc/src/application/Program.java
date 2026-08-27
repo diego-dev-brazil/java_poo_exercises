c6cb416b79add452625e055e9928f5099cf964da
diego <diegoaugustooliveira3@gmail.com>
2026-08-27 13:05:22 -0300

    Department entity created

---
+++ projeto_jdbc_cursonelio/projeto_jdbc/src/application/Program.java
@@ -0,0 +1,9 @@
+
+package application;
+import model.entities.Department;
+public class Program{
+	public static void main(String[] args) {
+		Department dep = new Department(1,"Vendas");
+		System.out.println(dep);
+	}
+}
