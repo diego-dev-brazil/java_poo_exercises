8be4cff99226339439fe222d3d073395240a7317
diego <diegoaugustooliveira3@gmail.com>
2026-09-08 12:39:34 -0300

    Trabalhando com interfaces funcionais

---
+++ DIO_Itaú_Bootcamp/Interfaces_Funcionais/Aula/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<User> users = List.of(new User ("Maria", 23), new User("Joao", 32), new User ("Ana", 19));
+		users.forEach(user -> System.out.println(user));
+		printStringValue(User::name, users);
+	}
+	private static void printStringValue (Function <User, String> callback, List<User> users) {
+		users.forEach(u -> System.out.println(callback.apply(u)));
+	}
+}
