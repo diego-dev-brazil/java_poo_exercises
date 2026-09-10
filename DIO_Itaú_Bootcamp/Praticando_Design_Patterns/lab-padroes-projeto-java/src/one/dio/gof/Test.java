2d98af0d9421d3fb3c91b3833f7032d2d25b11bc
diego <diegoaugustooliveira3@gmail.com>
2026-09-10 08:29:23 -0300

    Strategy project pattern practice, and Singleton files in their own package

--- DIO_Itaú_Bootcamp/Praticando_Design_Patterns/lab-padroes-projeto-java/src/one/dio/gof/Test.java
+++ DIO_Itaú_Bootcamp/Praticando_Design_Patterns/lab-padroes-projeto-java/src/one/dio/gof/Test.java
@@ -1,8 +1,20 @@
 package one.dio.gof;
 
+import one.dio.gof.singleton.SingletonEager;
+import one.dio.gof.singleton.SingletonLazy;
+import one.dio.gof.singleton.SingletonLazyHolder;
+import one.dio.gof.strategy.Comportamento;
+import one.dio.gof.strategy.ComportamentoAgressivo;
+import one.dio.gof.strategy.ComportamentoDefensivo;
+import one.dio.gof.strategy.ComportamentoNormal;
+import one.dio.gof.strategy.Robo;
+
 public class Test {
 
 	public static void main(String[] args) {
+		
+		//testes de singleton
+		System.out.println("\n========SINGLETON TESTS========\n");
 		SingletonLazy lazy = SingletonLazy.getInstancia();
 		SingletonLazy lazy2 = SingletonLazy.getInstancia();
 		SingletonEager eager = SingletonEager.getInstancia();
@@ -11,7 +23,22 @@ public class Test {
 		System.out.println(lazy2);
 		System.out.println(eager);
 		System.out.println(lazyholder);
-
+		
+		//testes de strategy
+		System.out.println("\n========STRATEGY TESTS========\n");
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.mover();
 	}
-
 }
--- DIO_Itaú_Bootcamp/Praticando_Design_Patterns/lab-padroes-projeto-java/src/one/dio/gof/singleton/SingletonEager.java
+++ DIO_Itaú_Bootcamp/Praticando_Design_Patterns/lab-padroes-projeto-java/src/one/dio/gof/singleton/SingletonEager.java
@@ -1,4 +1,4 @@
-package one.dio.gof;
+package one.dio.gof.singleton;
 
 /**
  * Singleton "apressado"
